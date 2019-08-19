06e71644527244280582a99f1707f4c09fed958c
FerTrabajoYotta <feryotta2@gmail.com>
2019-08-19 14:01:15 -0400

    crear una tarea persistente

---
+++ 04-por-hacer/por-hacer/por-hacer.js
@@ -0,0 +1,15 @@
+const fs = require("fs");
+let listadoPorHacer = [];
+
+const crear = descripcion => {
+  let porHacer = {
+    descripcion,
+    completado: false
+  };
+  listadoPorHacer.push(porHacer);
+  return porHacer;
+};
+
+module.exports = {
+  crear
+};
