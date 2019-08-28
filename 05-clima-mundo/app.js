2d37260f863f81090e00af9d78403004ef1de62d
TrabajoYottaFer <feryotta2@gmail.com>
2019-08-28 18:07:54 -0400

    Peticiones HTTP no finalizado

--- 05-clima-mundo/app.js
+++ 05-clima-mundo/app.js
@@ -1,3 +1,5 @@
+const axios = require("axios");
+
 const argv = require("yargs").options({
   direccion: {
     alias: "d",
@@ -7,3 +9,18 @@ const argv = require("yargs").options({
 }).argv;
 
 console.log(argv.direccion);
+
+// encodeURI permite eliminar espacio para la URL
+let encodedUrl = encodeURI(argv.direccion);
+
+axios
+  .get(`https://maps.googleapis.com/maps/api/geocode/json?address=${encodedUrl}&key=AIzaSyASEYwuLS1HDI3YAZ6w2gzVmZ8M-wUOG2M`)
+  .then(function (response) {
+    // handle success
+    console.log(JSON.stringify(response.data, undefined, 2));
+    // console.log(response.status);
+  })
+  .catch(function (error) {
+    // handle error
+    console.log(error);
+  });
