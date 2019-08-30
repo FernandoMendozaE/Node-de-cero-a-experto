2a5fb11385e242265cd250e58d761d46769e4d4f
Fernando <ferchome0@gmail.com>
2019-08-29 21:09:40 -0400

    Optimizando el codigo para obtener la dirección y coordenadas

--- 05-clima-mundo/app.js
+++ 05-clima-mundo/app.js
@@ -1,4 +1,4 @@
-const axios = require("axios");
+const lugar = require("./lugar/lugar");
 
 const argv = require("yargs").options({
   direccion: {
@@ -8,28 +8,9 @@ const argv = require("yargs").options({
   }
 }).argv;
 
-// console.log(argv.direccion);
-
-// encodeURI permite eliminar espacio para la URL
-let encodedUrl = encodeURI(argv.direccion);
-
-axios
-  .get(
-    `https://maps.googleapis.com/maps/api/geocode/json?address=${encodedUrl}&key=AIzaSyASEYwuLS1HDI3YAZ6w2gzVmZ8M-wUOG2M`
-  )
-  .then(function(resp) {
-    let location = resp.data.results[0];
-    let direccion = resp.data.results[0].formatted_address;
-    let coors = resp.data.results[0].geometry.location;
-
-    console.log("Dirección:", direccion);
-    console.log("lat:", coors.lat);
-    console.log("lng:", coors.lng);
-    // JSON convierte un [] en objeto parametros undefined=remplaza, 2=espaciado
-    //console.log(JSON.stringify(resp.data, undefined, 2));
-    // console.log(resp.status);
+lugar
+  .getLugarLatLng(argv.direccion)
+  .then(resp => {
+    console.log(resp);
   })
-  .catch(function(error) {
-    // handle error
-    console.log("ERROR!!!", error);
-  });
+  .catch(e => console.log(e));
