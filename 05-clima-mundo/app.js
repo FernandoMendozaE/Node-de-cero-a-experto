9e66dfc4f0dfcb58d778b54f11531c4f0701abe3
TrabajoYottaFer <feryotta2@gmail.com>
2019-08-30 08:26:36 -0400

    Creación de la carpeta lugar No completado

--- 05-clima-mundo/app.js
+++ 05-clima-mundo/app.js
@@ -1,4 +1,4 @@
-const axios = require("axios");
+
 
 const argv = require("yargs").options({
   direccion: {
@@ -11,25 +11,3 @@ const argv = require("yargs").options({
 // console.log(argv.direccion);
 
 // encodeURI permite eliminar espacio para la URL
-let encodedUrl = encodeURI(argv.direccion);
-
-axios
-  .get(
-    `https://maps.googleapis.com/maps/api/geocode/json?address=${encodedUrl}&key=AIzaSyASEYwuLS1HDI3YAZ6w2gzVmZ8M-wUOG2M`
-  )
-  .then(function(resp) {
-    let location = resp.data.results[0];
-    let direccion = resp.data.results[0].formatted_address;
-    let coors = resp.data.results[0].geometry.location;
-
-    console.log("Dirección:", direccion);
-    console.log("lat:", coors.lat);
-    console.log("lng:", coors.lng);
-    // JSON convierte un [] en objeto parametros undefined=remplaza, 2=espaciado
-    //console.log(JSON.stringify(resp.data, undefined, 2));
-    // console.log(resp.status);
-  })
-  .catch(function(error) {
-    // handle error
-    console.log("ERROR!!!", error);
-  });
