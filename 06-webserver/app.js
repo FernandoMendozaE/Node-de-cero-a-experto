f87eb9c96e669ef63eb46f35d85590b699bace9c
TrabajoYottaFer <feryotta2@gmail.com>
2019-08-30 13:58:12 -0400

    Inicio de proyecto-WebServer Ejemplo con un JSON

--- 06-webserver/app.js
+++ 06-webserver/app.js
@@ -2,7 +2,14 @@ const http = require("http");
 
 http
   .createServer((req, res) => {
-    res.write("Hola Mundo");
+    res.writeHead(200, { "Content-Type": "application/json" });
+
+    let salida = {
+      nombre: "Fernando",
+      edad: 25,
+      url: req.url
+    };
+    res.write(JSON.stringify(salida));
     res.end();
   })
   .listen(8080);
