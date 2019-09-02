38b6305db9c09bcfb1dc87d3aca6cda594bde88a
Fernando <ferchome0@gmail.com>
2019-09-01 22:00:03 -0400

    Servir contenido estatico

--- 06-webserver/server.js
+++ 06-webserver/server.js
@@ -1,16 +1,16 @@
 const express = require("express");
 const app = express();
 
-// respond with "hello world" when a GET request is made to the homepage
-app.get("/", (req, res) => {
-  // res.send("Hola Fernando");
-  let salida = {
-    nombre: "Fernando",
-    edad: 25,
-    url: req.url
-  };
-  res.send(salida);
-});
+app.use(express.static(__dirname + "/public"));
+
+// app.get("/", (req, res) => {
+//   let salida = {
+//     nombre: "Fernando",
+//     edad: 25,
+//     url: req.url
+//   };
+//   res.send(salida);
+// });
 
 app.get("/data", (req, res) => {
   res.send("Hola Data");
